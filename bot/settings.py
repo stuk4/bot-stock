639c53bc52b264deedc916e0cd8f04ebd747c524
Bastian Didier Pérez <43711458+stuk4@users.noreply.github.com>
2023-01-19 21:03:17 -0300

    Update settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -48,8 +48,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bbasti390@gmail.com'
-EMAIL_HOST_PASSWORD = 'nymeria1'
+EMAIL_HOST_USER = '<email>@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
